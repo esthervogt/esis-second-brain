e6f2fe74f926347d1e2cb3fc2be8a972bb2efc73
esthervogt <esther-vogt@gmx.net>
2022-12-29 22:41:42 +0100

    Added main to habit tracker

--- notion/add_habit_to_tracker.py
+++ notion/add_habit_to_tracker.py
@@ -3,37 +3,39 @@ import notion.utils as u
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+if __name__ == "__main__":
 
-# TODO: support deletion of properties
+    load_dotenv()
 
-# get source df
-databaseId_src = os.getenv('DB_ID_HABITS')
-habits_db = u.read_database(databaseId_src)
-habits_df = pd.json_normalize(habits_db, record_path=['results'])
+    # TODO: support deletion of properties
 
-# filter on entries relevant for tracking
-habits_df = habits_df[(habits_df['properties.Add to tracking.checkbox'] == True) &
-                      (habits_df['properties.Frequency.select.name'] == 'daily') &
-                      (habits_df['archived'] == False)]
+    # get source df
+    databaseId_src = os.getenv('DB_ID_HABITS')
+    habits_db = u.read_database(databaseId_src)
+    habits_df = pd.json_normalize(habits_db, record_path=['results'])
 
-# get page title to be added to tracking
-habit_titles = habits_df['id'].apply(u.read_page).apply(u.get_page_title)
+    # filter on entries relevant for tracking
+    habits_df = habits_df[(habits_df['properties.Add to tracking.checkbox'] == True) &
+                          (habits_df['properties.Frequency.select.name'] == 'daily') &
+                          (habits_df['archived'] == False)]
 
-# get target df
-databaseId_target = os.getenv('DB_ID_HABIT_TRACKER')
-habits_tracker_db = u.read_database(databaseId_target)
-habits_tracker_df = pd.json_normalize(habits_tracker_db, record_path=['results'])
-habits_tracked = set(
-    [i.replace('properties.', '').split('.')[0] for i in habits_tracker_df.columns if 'properties' in i])
+    # get page title to be added to tracking
+    habit_titles = habits_df['id'].apply(u.read_page).apply(u.get_page_title)
 
-# filter out existing habits
-habits_to_add = [i for i in habit_titles if i not in habits_tracked]
+    # get target df
+    databaseId_target = os.getenv('DB_ID_HABIT_TRACKER')
+    habits_tracker_db = u.read_database(databaseId_target)
+    habits_tracker_df = pd.json_normalize(habits_tracker_db, record_path=['results'])
+    habits_tracked = set(
+        [i.replace('properties.', '').split('.')[0] for i in habits_tracker_df.columns if 'properties' in i])
 
-# add habits to tracker db
-if len(habits_to_add) > 0:
-    update_responses = {i: u.update_database(databaseId_target, properties={i: {'checkbox': {}}}) for i in
-                        habits_to_add}
-    print(f'Status codes for responses added to tracker: {update_responses}')
-else:
-    print(f'No new habits found to add to tracker.')
+    # filter out existing habits
+    habits_to_add = [i for i in habit_titles if i not in habits_tracked]
+
+    # add habits to tracker db
+    if len(habits_to_add) > 0:
+        update_responses = {i: u.update_database(databaseId_target, properties={i: {'checkbox': {}}}) for i in
+                            habits_to_add}
+        print(f'Status codes for responses added to tracker: {update_responses}')
+    else:
+        print(f'No new habits found to add to tracker.')
